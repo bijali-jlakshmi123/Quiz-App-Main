2b8e5bf702bb3a7b69820376ae77ff382eb83525
Bijali Jayalakshmi Jayan <120201248+bijali-jlakshmi123@users.noreply.github.com>
2026-01-05 20:30:25 +0530

    changes updated

--- client/src/pages/common/Login/index.js
+++ client/src/pages/common/Login/index.js
@@ -15,7 +15,7 @@ function Login() {
       if (response.success) {
         message.success(response.message);
         localStorage.setItem("token", response.data);
-        window.location.href = "/";
+        window.location.href = "/admin/exams";
       } else {
         message.error(response.message);
       }
